293832022924d7f49f71249da7bfcf1b68f1070e
tmakdrl <tmakdrl@naver.com>
2024-01-22 20:51:10 +0900

    updated

--- 1181/main.cpp
+++ 1181/main.cpp
@@ -7,37 +7,39 @@ using namespace std;
 
 void Init()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 }
 
 bool cmp(string a, string b)
 {
-    if (a.length() == b.length())
-        return a < b;
-    else
-        return a.length() < b.length();
+	if (a.length() == b.length())
+		return a < b;
+	else
+		return a.length() < b.length();
 }
 
 int main()
 {
-    Init();
-    int N;
-    cin >> N;
+	Init();
+	int N;
+	cin >> N;
 
-    vector<string> v;
-    string s;
+	vector<string> v;
+	string s;
 
-    for (int i = 0; i < N; i++)
-    {
-        cin >> s;
-        v.push_back(s);
-    }
+	for (int i = 0; i < N; i++)
+	{
+		cin >> s;
+		v.push_back(s);
+	}
 
-    sort(v.begin(), v.end(), cmp);
+	sort(v.begin(), v.end(), cmp);
+	v.erase(unique(v.begin(), v.end()), v.end());
 
-    for (int i = 0; i < N; i++)
-        cout << v[i] << endl;
-    return 0;
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i] << endl;
+
+	return 0;
 }
