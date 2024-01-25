f517d0b692ae5c8574a170c962d5458070bc8351
tmakdrl <tmakdrl@naver.com>
2024-01-25 23:35:48 +0900

    updated

---
+++ 1018/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#define endl '\n'
+using namespace std;
+
+void Init()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+int main()
+{
+	Init();
+	int m, n;
+	cin >> m >> n;
+	char arr[51][51];
+	for (int i = 0; i < 51; i++)
+		for (int j = 0; j < 51; j++)
+			cin >> char[i][j];
+
+		return 0;
+}
--- 11866/main.cpp
+++ 11866/main.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
+#include <queue>
 #define endl '\n'
 using namespace std;
 
 void Init()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 }
 
 int main()
 {
-    Init();
+	Init();
+	int n, k;
+	cin >> n >> k;
+	queue<int> q;
 
-    return 0;
+	for (int i = 1; i <= n; i++)
+		q.push(i);
+
+	cout << '<';
+	while (q.size() != 1)
+	{
+		for (int i = 0; i < k - 1; i++)
+		{
+			int t = q.front();
+			q.pop();
+			q.push(t);
+		}
+		cout << q.front() << ", ";
+		q.pop();
+	}
+
+	cout << q.front() << '>' << endl;
+	return 0;
 }
