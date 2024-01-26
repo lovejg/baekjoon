4a8bbc6d53980dab5242a2db266e23612ccfbdbc
tmakdrl <tmakdrl@naver.com>
2024-01-26 12:54:01 +0900

    updated

--- 1018/main.cpp
+++ 1018/main.cpp
@@ -2,6 +2,8 @@
 #define endl '\n'
 using namespace std;
 
+char arr[51][51];
+
 void Init()
 {
 	ios::sync_with_stdio(false);
@@ -9,15 +11,59 @@ void Init()
 	cout.tie(NULL);
 }
 
+int cal(int m, int n)
+{
+	int black = 0;
+	int white = 0;
+
+	bool bw = false;
+	if (arr[m][n] == 'B') // 좌상단이 black이면
+		bw = true;
+
+	for (int i = m; i < m + 8; i++)
+	{
+		for (int j = n; j < n + 8; j++)
+		{
+			if (bw)
+			{
+				if (((i + j) % 2 == 0 && arr[i][j] == 'W') || ((i + j) % 2 == 1 && arr[i][j] == 'B'))
+					black++;
+				else if (((i + j) % 2 == 0 && arr[i][j] == 'B') || ((i + j) % 2 == 1 && arr[i][j] == 'W'))
+					white++;
+			}
+			else
+			{
+				if (((i + j) % 2 == 0 && arr[i][j] == 'B') || ((i + j) % 2 == 1 && arr[i][j] == 'W'))
+					black++;
+				else if (((i + j) % 2 == 0 && arr[i][j] == 'W') || ((i + j) % 2 == 1 && arr[i][j] == 'B'))
+					white++;
+			}
+		}
+	}
+
+	return min(black, white);
+}
+
 int main()
 {
 	Init();
 	int m, n;
 	cin >> m >> n;
-	char arr[51][51];
-	for (int i = 0; i < 51; i++)
-		for (int j = 0; j < 51; j++)
-			cin >> char[i][j];
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			cin >> arr[i][j];
 
-		return 0;
+	int min = 2500; // 50 x 50
+	for (int i = 0; i < m - 7; i++)
+	{
+		for (int j = 0; j < n - 7; j++)
+		{
+			int cost = cal(i, j);
+			if (cost < min)
+				min = cost;
+		}
+	}
+
+	cout << min << endl;
+	return 0;
 }
