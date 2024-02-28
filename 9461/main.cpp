45f369821ad5e03f49a0db387a8b896c3dbbd005
tmakdrl <tmakdrl@naver.com>
2024-02-28 21:57:10 +0900

    updated

---
+++ 9461/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#define endl '\n'
+using namespace std;
+
+void Init()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main()
+{
+    Init();
+    int T;
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        int n;
+        cin >> n;
+    }
+    return 0;
+}
