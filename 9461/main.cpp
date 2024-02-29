e6692bc9285bde14fc2760f2f067567263ca3464
tmakdrl <tmakdrl@naver.com>
2024-02-29 21:51:59 +0900

    updated

--- 9461/main.cpp
+++ 9461/main.cpp
@@ -14,11 +14,26 @@ int main()
     Init();
     int T;
     cin >> T;
+    long long dp[101];
+    dp[1] = 1;
+    dp[2] = 1;
+    dp[3] = 1;
+    dp[4] = 2;
+    dp[5] = 2;
+    dp[6] = 3;
+    dp[7] = 4;
+    dp[8] = 5;
+    dp[9] = 7;
+
+    for (int i = 10; i < 101; i++)
+        dp[i] = dp[i - 1] + dp[i - 5];
 
     for (int i = 0; i < T; i++)
     {
         int n;
         cin >> n;
+        cout << dp[n] << endl;
     }
+
     return 0;
 }
