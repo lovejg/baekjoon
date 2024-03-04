5ec5df58d9c2795edbcb73076d241c3074e49dd5
tmakdrl <tmakdrl@naver.com>
2024-03-04 13:19:49 +0900

    updated

--- 11659/main.cpp
+++ 11659/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #define endl '\n'
 using namespace std;
 
@@ -12,6 +13,22 @@ void Init()
 int main()
 {
     Init();
+    int n, m;
+    cin >> n >> m;
 
+    vector<int> v;
+
+    for (int i = 0; i < n; i++)
+    {
+        int k;
+        cin >> k;
+        v.push_back(k);
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+    }
     return 0;
 }
