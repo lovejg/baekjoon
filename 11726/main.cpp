e837c4921b6f0b8b84130e52a08c1df1c915b241
tmakdrl <tmakdrl@naver.com>
2024-07-20 15:51:02 +0900

    updated

--- 11726/main.cpp
+++ 11726/main.cpp
@@ -15,5 +15,18 @@ int main()
     int n;
     cin >> n;
 
+    int dp[1001];
+
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 3;
+
+    for (int i = 4; i <= n; i++)
+    {
+        dp[i] = max(dp[i - 1], dp[i - 2]) % 10007;
+    }
+
+    cout << dp[n] << endl;
     return 0;
 }
