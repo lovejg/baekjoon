598ba33400bc4ee71692e04f5427080338242ecc
tmakdrl <tmakdrl@naver.com>
2024-07-26 17:15:41 +0900

    updated

--- 2805/main.cpp
+++ 2805/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #define endl '\n'
 using namespace std;
 
@@ -12,6 +14,39 @@ void Init()
 int main()
 {
     Init();
+    int n, m;
+    cin >> n >> m;
+    vector<int> v;
 
+    for (int i = 0; i < n; i++)
+    {
+        int t;
+        cin >> t;
+        v.push_back(t);
+    }
+
+    sort(v.begin(), v.end());
+
+    long long start = 0; // 최소 높이는 v[0]가 아니라 0임에 주목
+    long long end = v[v.size() - 1];
+
+    int max = -1;
+    while (start <= end)
+    {
+        long long mid = (start + end) / 2;
+        long long temp = 0;
+        for (int i = 0; i < v.size(); i++)
+            if (v[i] - mid > 0)
+                temp += (v[i] - mid);
+        if (temp >= m)
+        {
+            if (max < mid) // 구해야 되는게 temp가 아닌 mid, 즉 높이를 구해야된다는 점 명심
+                max = mid;
+            start = mid + 1;
+        }
+        else
+            end = mid - 1;
+    }
+    cout << max << endl;
     return 0;
 }
