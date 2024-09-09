5e2c4e0f9a9f569e3f81cdb3d133d48e912b684b
tmakdrl <tmakdrl@naver.com>
2024-09-09 15:58:09 +0900

    solving 6064

---
+++ 6064/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#define endl '\n'
+using namespace std;
+
+void Init()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main()
+{
+    Init();
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        int m,n,x,y;
+        cin>>m>>n>>x>>y;
+        
+    }
+    return 0;
+}
